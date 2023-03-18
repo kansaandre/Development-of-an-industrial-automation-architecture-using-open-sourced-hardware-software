92921e7e3cbb09949bf13e33dbf496f9619ed1d1
kansaandre <kansaandre@gmail.com>
2023-03-18 22:06:17 +0100

    Update Batch-Process.ino
    The code is an Arduino sketch that communicates with Node-RED using JSON messages. It simulates a batch process with various states, handling sensor and actuator values. The sketch receives sensor data, processes it based on a state machine, updates actuator values, and sends the actuator states back to Node-RED.

--- arduino_sourcecode/Batch-Process/Batch-Process.ino
+++ arduino_sourcecode/Batch-Process/Batch-Process.ino
@@ -1,115 +1,144 @@
+#include <ArduinoJson.h>
+#include <arduino-timer.h>
+
+// Variables
+bool heater = false;
+bool stirrer = false;
+bool valve_a = false;
+bool valve_b = false;
+bool valve_c = false;
+bool s1 = true;
+bool s2 = true;
+bool s3 = true;
+float temp = 50.0;
+bool start = false;
+bool stop1 = false;
+bool stop2 = false;
+
+// Timer
+auto timer = timer_create_default();
 
 void setup() {
   Serial.begin(9600);
-  }
- 
-void loop() {
+  timer.every(100, processControlLogic, nullptr); // Pass nullptr as a second argument
+}
 
+void loop() {
+  receiveData();
+  sendData();
+  timer.tick();
+}
+
+void receiveData() {
+  if (Serial.available()) {
+    StaticJsonDocument<256> doc;
+    DeserializationError error = deserializeJson(doc, Serial);
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    bool simulate = doc["simulate"];
+
+    if (simulate) {
+      s1 = doc["sim_s1"];
+      s2 = doc["sim_s2"];
+      s3 = doc["sim_s3"];
+      temp = doc["sim_temp"];
+    } else {
+      s1 = doc["s1"];
+      s2 = doc["s2"];
+      s3 = doc["s3"];
+      temp = doc["temp"];
+    }
+
+    start = doc["start"];
+    stop1 = doc["stop1"];
+    stop2 = doc["stop2"];
+  }
+}
+
+void sendData() {
+  StaticJsonDocument<256> doc;
+
+  doc["heater"] = heater;
+  doc["stirrer"] = stirrer;
+  doc["valve_a"] = valve_a;
+  doc["valve_b"] = valve_b;
+  doc["valve_c"] = valve_c;
+
+  serializeJson(doc, Serial);
+  Serial.println();
+}
+
+bool processControlLogic(void*) { // Update the function signature
   enum state {
     ready = 0,
     fill_a = 1,
     fill_b = 2,
     heating = 3,
-    wait = 4, 
+    wait = 4,
     drain1 = 5,
     drain2 = 6
   };
-  
-  
-  bool heater = false;
-  bool s1 = true; // Normally Closed (NC)
-  bool s2 = true; // NC
-  bool s3 = true; // NC
-  bool start = false;
-  bool stirrer = false;
-  bool stop1 = false;
-  bool stop2 = false;
-  bool valve_a = false;
-  bool valve_b = false;
-  bool valve_c = false;
 
-  unsigned int counter = 0;
-  float temp = 50.0; //[C] Set to a value within the process area 
-  unsigned long int elapsed_millis = 0; //[ms] time logged at case wait  
-  state current_state = ready;
-  //-------------------------------------
+  static state current_state = ready;
 
   switch (current_state) {
-
-    case ready: 
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      counter = 0; // Reset counter
-      if(start == true) {
-        current_state = fill_a; // Change state name
+    case ready:
+      if (start) {
+        current_state = fill_a;
       }
-      break;  // Jump out of ready case
+      break;
 
     case fill_a:
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      counter = counter + 1; // Counting loop cycles
-      valve_a = true; // Open valve A letting in substance A 
-      if(s2 == false or stop2 == true) { // When tank reach s2 then do...
-        current_state = fill_b; // Change state name
-       }
-      break; // Jump out of fill_a case
+      valve_a = true;
+      if (!s2 || stop2) {
+        current_state = fill_b;
+      }
+      break;
 
     case fill_b:
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      valve_a = false; // Close valve A from letting in substance A
-      valve_b = true; // Open valve B letting in substance B
-      heater = true; // Start heating the substances in the tank
-      stirrer = true; // Start stirring the tank substances
-      if(s3 == false or stop2 == true) { // When tank reach s3 then do...
-        current_state = heating; // Change state name
+      valve_a = false;
+      valve_b = true;
+      heater = true;
+      stirrer = true;
+      if (!s3 || stop2) {
+        current_state = heating;
       }
-      break; // Jump out of fill_b case
-      
+      break;
+
     case heating:
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      valve_b = false; // Close valve B from letting in substance B
-      if(temp > 85 or stop2 == true) {
-        current_state = wait; // Change state name
+      valve_b = false;
+      if (temp > 85 || stop2) {
+        current_state = wait;
       }
-      break; // Jump out of heating case
+      break;
 
     case wait:
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      elapsed_millis = millis(); // Log time elapsed as soon as case is activated
-      if((millis()-elapsed_millis) > 30000 or stop2 == true) { // Wait until 30 s has elapsed then do...
-        current_state = drain1; // Change state name
+      heater = false;
+      stirrer = false;
+      if (stop1) {
+        current_state = drain1;
       }
-      break; // Jump out of wait case
+      break;
 
     case drain1:
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      heater = false; // Stop heating of tank
-      valve_c = true; // Open valve C letting out mixture of A and B from tank
-      if(s2 == true or stop2 == true) { // When tank level fall below s2 then do...
-        current_state = drain2; // Change state name
+      valve_c = true;
+      if (!s1 || stop1) {
+        current_state = drain2;
       }
-      break; // Jump out of drain1 case
+      break;
 
     case drain2:
-      Serial.print("Current state: ");
-      Serial.println(current_state);
-      stirrer = false; // Stop stirring the tank substances
-      if((s1 == true) and (counter < 10) and (stop1 == false and stop2 == false)) {
-        current_state = fill_a; // must complete 10 cycles or be stopped to go back to ready state
-      }
-      else {
-        current_state = ready; // if stop or 10 cycles have been complete jump to readystate
-      }
-      break; // Jump out of drain2 case
-      
-  
-    //hello world 
-   //hello andre 
-       
+      valve_c = false;
+      current_state = ready;
+      break;
+         default:
+      current_state = ready;
+      break;
   }
+  return true;
 }
